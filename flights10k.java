57db0b127a40693b0b27158fca5090549315de71
liuk-6 <liuk6@tcd.ie>
2026-03-12 10:25:31 +0000

    Flights10k.java

---
+++ flights10k.java
@@ -0,0 +1,25 @@
+
+import java.io.File;
+import java.util.Scanner;
+
+class flights10k{
+
+    public static void main(String[] args) {
+
+        try {
+            File file = new File("flights10k.csv");
+            Scanner scanner = new Scanner(file);
+
+            while(scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                System.out.println(line);
+            }
+
+            scanner.close();
+
+        } catch (Exception e) {
+            System.out.println("Error reading file");
+        }
+    }
+}
+
