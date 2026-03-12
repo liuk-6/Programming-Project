c4209e1c54ebceb4215815e0c28338651bcc7135
alemans-cell <alemans@tcd.ie>
2026-03-12 04:43:31 -0600

    Create ReadingFile100k.java

---
+++ ReadingFile100k.java
@@ -0,0 +1,24 @@
+import java.io.File;
+import java.util.Scanner;
+
+class flights10k{
+
+    public static void main(String[] args) {
+
+        try {
+            File file = new File("flights100k.csv");
+            Scanner scanner = new Scanner(file);
+
+            while(scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                System.out.println(line);
+            }
+
+            scanner.close();
+
+        } catch (Exception e) {
+            System.out.println("Error reading file");
+        }
+    }
+}
+
